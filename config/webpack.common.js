2b07df2c3b54abe783622c0a1f1f6902e4f719d0
syarif <syarif.ibrohim@bukalapak.com>
2025-06-27 10:12:22 +0700

    init vue + webpack

---
+++ config/webpack.common.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {
+  VueLoaderPlugin
+} = require('vue-loader');
+
+const __base = path.resolve(__dirname, '..');
+const __src = path.resolve(__base, 'src');
+
+module.exports = {
+  entry: path.resolve(__src, 'app.js'),
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__base, 'dist'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader']
+    }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Vue - Profile',
+      template: path.resolve(__src, 'index.html'),
+    }),
+    new VueLoaderPlugin()
+  ]
+}
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Vue - Profile</h1>
+</template>
+
+<script>
+
+import { defineComponent } from 'vue'; 
+
+export default defineComponent({
+  name: 'App'
+});
+
+</script>
